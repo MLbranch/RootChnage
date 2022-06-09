c29d9cd80eae12220dab48f9262fb3abbacc7e81
Qazi Ammar Arshad <qaziammar.g@gmail.com>
2022-06-09 16:22:47 +0500

    Initial Commit

---
+++ RootChnage/RootChnageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RootChnageApp.swift
+//  RootChnage
+//
+//  Created by Qazi Ammar Arshad on 09/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct RootChnageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
